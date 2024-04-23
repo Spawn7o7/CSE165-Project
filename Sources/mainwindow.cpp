e575e9df12bfb37964e78f02da81f10de4053320
Spawn7o7 <122921549+Spawn7o7@users.noreply.github.com>
2024-04-23 13:19:58 -0700

    further defined classes
    defined and created "createActions", "createMenu", "quickSave", "saveFile"

--- Sources/mainwindow.cpp
+++ Sources/mainwindow.cpp
@@ -4,6 +4,8 @@
 #include "drawingspace.h"
 #include "ui_mainwindow.h"
 
+//might need to comment out UI window if it clashes with app code, have not run and tested yet.
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -17,6 +19,7 @@ MainWindow::MainWindow(QWidget *parent)
     setWindowTitle(tr("Totally Not MS Paint"));
 }
 
+
 void MainWindow::closeEvent(QCloseEvent *event){ //prompts user to save before they fully close out of app
     if(quickSave()){
         event->accept();
@@ -26,8 +29,9 @@ void MainWindow::closeEvent(QCloseEvent *event){ //prompts user to save before t
     }
 }
 
+
 void MainWindow::open(){
-    if(quickSave()){
+    if(quickSave()){ // If save file found, allow user to select it or start a new drawing
         QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), QDir::currentPath());
 
         if(!fileName.isEmpty()){
@@ -36,11 +40,13 @@ void MainWindow::open(){
     }
 }
 
+
 void MainWindow::save(){
     QAction *action = qobject_cast<QAction *>(sender());
     QByteArray fileFormat = action->data().toByteArray();
     saveFile(fileFormat);
 }
+
 
 void MainWindow::penColor(){
     QColor newColor = QColorDialog::getColor(drawingSpace->penColor());
@@ -48,6 +54,7 @@ void MainWindow::penColor(){
         drawingSpace->setPenColor(newColor);
     }
 }
+
 
 void MainWindow::penSize(){
     bool confirm;
@@ -57,6 +64,101 @@ void MainWindow::penSize(){
         drawingSpace->setPenSize(newSize);
     }
 }
+
+
+void MainWindow::createActions(){
+    openAction = new QAction(tr("&Open"), this);
+    openAction->setShortcuts(QKeySequence::Open);
+    connect(openAction, SIGNAL(triggered(bool)), this, SLOT(open()));
+
+    // Save drawing (choosing format type)
+    foreach(QByteArray format, QImageWriter::supportedImageFormats()){
+        QString text = tr("%1...").arg(QString(format).toUpper());
+        QAction *action = new QAction(text, this);
+        action->setData(format);
+        connect(action, SIGNAL(triggered()), this, SLOT(save()));
+        
+        saveAsAction.append(action);
+    }
+
+    // Selecting to print image
+    printAction = new QAction(tr("&Print..."), this);
+    connect(printAction, SIGNAL(triggered()), drawingSpace, SLOT(print()));
+
+    // Quitting the drawing program
+    closeAction = new QAction(tr("&Exit"), this);
+    closeAction->setShortcuts(QKeySequence::Quit);
+    connect(closeAction, SIGNAL(triggered()), this, SLOT(close()));
+
+    // Pen Customization
+    penColorAction = new QAction(tr("&Pen Color..."), this);
+    connect(penColorAction, SIGNAL(triggered()), drawingSpace, SLOT(penColor()));
+    penSizeAction = new QAction(tr("Pen &Size..."), this);
+    connect(penSizeAction, SIGNAL(triggered()), drawingSpace, SLOT(penSize()));
+
+    // An action that clears the screen
+    deleteAllAction = new QAction(tr("&Delete All..."), this);
+    deleteAllAction->setShortcut(tr("Ctrl+L"));
+    connect(deleteAllAction, SIGNAL(triggered()), drawingSpace, SLOT(clearImage()));
+}
+
+
+void MainWindow::createMenu(){
+    saveAs = new QMenu(tr("&Save As"), this);
+    foreach(QAction *action, saveAsAction){
+        saveAs->addAction(action);
+    }
+
+    fileMenu = new QMenu(tr("&File"), this);
+    fileMenu->addAction(openAction);
+    //fileMenu->addAction(saveAs); // Error: no matching member function?
+    fileMenu->addAction(printAction);
+    fileMenu->addSeparator();
+    fileMenu->addAction(closeAction);
+
+    brushOptions = new QMenu(tr("&Brush"), this);
+    brushOptions->addAction(penColorAction);
+    brushOptions->addAction(penSizeAction);
+    brushOptions->addSeparator();
+    brushOptions->addAction(deleteAllAction);
+
+    menuBar()->addMenu(fileMenu);
+    menuBar()->addMenu(brushOptions);
+}
+
+
+bool MainWindow::quickSave(){
+    if(drawingSpace->isModified()){
+        QMessageBox::StandardButton prompt;
+        prompt = QMessageBox::warning(this, tr("Totally Not MS Paint"),
+                                   tr("Do you want to save your work?\n"
+                                         "There are unsaved changes."),
+                                   QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+        if(prompt == QMessageBox::Save){
+            return saveFile("png");
+        }
+        else if(prompt == QMessageBox::Cancel){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+bool MainWindow::saveFile(const QByteArray &fileFormat){
+    QString initialPath = QDir::currentPath() + "/untitled." + fileFormat;
+    QString fileName = QFileDialog::getSaveFileName(this, tr("Save As"), initialPath,
+                                                    tr("%1 Files (*.%2);; All Files(*)")
+                                                    .arg(QString::fromLatin1(fileFormat.toUpper()))
+                                                        .arg(QString::fromLatin1(fileFormat)));
+    if(fileName.isEmpty()){
+        return false;
+    }
+    else{
+        return drawingSpace->saveImage(fileName, fileFormat.constData());
+    }
+}
+
 
 MainWindow::~MainWindow()
 {
