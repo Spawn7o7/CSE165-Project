2222a3ffe42ac85626e30a29d273cef8bde95cd6
Spawn7o7 <122921549+Spawn7o7@users.noreply.github.com>
2024-04-22 21:02:47 -0700

    Basic class outlines implemented

---
+++ drawingspace.h
@@ -0,0 +1,51 @@
+#ifndef DRAWINGSPACE_H
+#define DRAWINGSPACE_H
+
+#include <QColor>
+#include <QImage>
+#include <QPoint>
+#include <QWidget>
+
+class DrawingSpace : public QWidget{
+    Q_OBJECT
+
+public:
+    DrawingSpace(QWidget *parent = 0);
+    bool openImage(const QString &fileName);
+    bool saveImage(const QString &fileName, const char *fileFormat);
+
+    void setPenColor(const QColor &newColor);
+    void setPenSize(int newSize);
+
+    bool isModified() const {return modified;}
+    QColor penColor() const {return myPenColor;}
+    int penSize() const {return myPenSize;}
+
+public slots:
+    void clearImage();
+    void print();
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+    void paintEvent(QPaintEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
+
+private:
+    void drawLineTo(const QPoint &endPoint);
+    void resizeImage(QImage *image, const QSize &newSize);
+
+    bool modified;
+    bool drawing;
+
+    QColor myPenColor;
+    int myPenSize;
+    QImage image;
+    QPoint lastPoint;
+
+};
+
+
+#endif // DRAWINGSPACE_H
